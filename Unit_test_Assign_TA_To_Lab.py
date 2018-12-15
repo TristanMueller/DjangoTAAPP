58df8e0629bf957a2d88f5dcd1f3bbe95a9b4d64
dummer-isaiah <44410836+dummer-isaiah@users.noreply.github.com>
2018-12-14 18:12:35 -0600

    Divided the Unit Tests into seperate Python Files

---
+++ Unit_test_Assign_TA_To_Lab.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from TASchedulingApp import TASchedulingApp
+from User import User
+
+
+class Testcode(TestCase):
+    App = TASchedulingApp()
+
+    def test_AssignTAToLab_valid(self):
+        self.App.LoggedInUser = User("Admin", "Admin", 1)
+        self.App.createAccount("Tristan", "Tristan", 3)
+        self.App.createCourse("12345", "CS361", "Tristan")
+        self.App.createAccount("Assistant", "Assistant", 4)
+        self.App.logout()
+        self.App.login("Tristan", "Tristan")
+        self.App.createLab("001", "12345", "")
+        self.assertTrue(self.App.assignTAToLab("001", "Assistant"))
+
+    def test_AssignTAToLab_NotAProfessor(self):
+        self.App.LoggedInUser = User("Admin", "Admin", 1)
+        self.App.createAccount("Tristan", "Tristan", 3)
+        self.App.createCourse("12345", "CS361", "Tristan")
+        self.App.createAccount("Assistant", "Assistant", 4)
+        self.App.createLab("001", "12345", "")
+        self.assertFalse(self.App.assignTAToLab("001", "Assistant"))
+
+    def test_AssignTAToLab_WrongProfessor(self):
+        self.App.LoggedInUser = User("Admin", "Admin", 1)
+        self.App.createAccount("Tristan", "Tristan", 3)
+        self.App.createCourse("12345", "CS361", "Tristan")
+        self.App.createAccount("Assistant", "Assistant", 4)
+        self.App.createAccount("John", "John", 3)
+        self.App.logout()
+        self.App.login("Tristan", "Tristan")
+        self.App.createLab("001", "12345", "")
+        self.App.logout()
+        self.App.login("John", "John")
+        self.assertFalse(self.App.assignTAToLab("001", "Assistant"))
+
+    def test_AssignTAToLab_TADoesntExist(self):
+        self.App.LoggedInUser = User("Admin", "Admin", 1)
+        self.App.createAccount("Tristan", "Tristan", 3)
+        self.App.createCourse("12345", "CS361", "Tristan")
+        self.App.createAccount("John", "John", 3)
+        self.App.logout()
+        self.App.login("Tristan", "Tristan")
+        self.App.createLab("001", "12345", "")
+        self.assertFalse(self.App.assignTAToLab("001", "Assistant"))
